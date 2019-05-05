ad82837b6d8edff0d83a08c65acfce18ea9fd901
olathenorthTSA <49179193+olathenorthTSA@users.noreply.github.com>
2019-05-04 22:25:04 -0500

    Update ticket.js

--- ticket.js
+++ ticket.js
@@ -9,3 +9,18 @@ window.onclick = function(event) {
 
 function closeForm() {
   document.getElementById("id01").style.display = "none";
+}
+
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function closeForm() {
+  document.getElementById("id02").style.display = "none";
+}
+
